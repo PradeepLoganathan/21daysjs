9180617c9131babe7c1a9567e47f8394013b8b2c
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2017-03-04 19:41:01 +0530

    HTML integration

--- recipe.js
+++ recipe.js
@@ -24,7 +24,17 @@ var recipe =
 
     displayRecipe: function()
     {
-        console.log(this.recipesteps);
+        if( this.recipesteps.length == 0)
+        {
+            console.log('recipe steps are empty');
+        }
+        else
+        {
+            for(var i=0; i < this.recipesteps.length; i++)
+            {
+                console.log(this.recipesteps[i].stepname + ' | ' + this.recipesteps[i].completed);
+            }
+        }
     },
 
     toggleCompleted: function(stepnumber)
@@ -34,26 +44,31 @@ var recipe =
 
 };
 
-console.log('*** Adding recipe steps ***');
-recipe.addRecipeStep('get flour');
-recipe.addRecipeStep('add yeast');
-recipe.addRecipeStep('add sugar');
-recipe.displayRecipe();
-console.log('*** Completed adding recipe steps ***' + '\n');
 
 
-console.log('*** Changing recipe step 2 ***');
-recipe.changeRecipestep(2, 'add soda');
-recipe.displayRecipe();
-console.log('*** Completed changing recipe step 2 ***' + '\n');
+// var btndisplayrecipes = document.getElementById('btndisplayrecipe');
+// btndisplayrecipes.addEventListener("click", function() { recipe.displayRecipe();});
+// recipe.displayRecipe();
+// console.log('*** Adding recipe steps ***');
+// recipe.addRecipeStep('get flour');
+// recipe.addRecipeStep('add yeast');
+// recipe.addRecipeStep('add sugar');
+// recipe.displayRecipe();
+// console.log('*** Completed adding recipe steps ***' + '\n');
 
-console.log('*** mark step 1 as completed ***');
-recipe.toggleCompleted(1)
-recipe.displayRecipe();
-console.log('*** completed mark step 1 as completed ***' + '\n');
+
+// console.log('*** Changing recipe step 2 ***');
+// recipe.changeRecipestep(2, 'add soda');
+// recipe.displayRecipe();
+// console.log('*** Completed changing recipe step 2 ***' + '\n');
+
+// console.log('*** mark step 1 as completed ***');
+// recipe.toggleCompleted(1)
+// recipe.displayRecipe();
+// console.log('*** completed mark step 1 as completed ***' + '\n');
 
 
-console.log('*** Remove recipe step2 ***');
-recipe.removeRecipeStep(2)
-recipe.displayRecipe();
-console.log('***Completed  removing recipe step2 ***' + '\n');
+// console.log('*** Remove recipe step2 ***');
+// recipe.removeRecipeStep(2)
+// recipe.displayRecipe();
+// console.log('***Completed  removing recipe step2 ***' + '\n');

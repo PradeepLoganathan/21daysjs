283f5620d10a0a086156eeba10ca5db554695451
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2017-03-04 13:03:52 +0530

    console printing and objects

--- recipe.js
+++ recipe.js
@@ -25,15 +25,35 @@ var recipe =
     displayRecipe: function()
     {
         console.log(this.recipesteps);
+    },
+
+    toggleCompleted: function(stepnumber)
+    {
+        this.recipesteps[stepnumber-1].completed = !this.recipesteps[stepnumber-1].completed;
     }
 
 };
 
+console.log('*** Adding recipe steps ***');
 recipe.addRecipeStep('get flour');
 recipe.addRecipeStep('add yeast');
 recipe.addRecipeStep('add sugar');
 recipe.displayRecipe();
+console.log('*** Completed adding recipe steps ***' + '\n');
+
+
+console.log('*** Changing recipe step 2 ***');
 recipe.changeRecipestep(2, 'add soda');
 recipe.displayRecipe();
+console.log('*** Completed changing recipe step 2 ***' + '\n');
+
+console.log('*** mark step 1 as completed ***');
+recipe.toggleCompleted(1)
+recipe.displayRecipe();
+console.log('*** completed mark step 1 as completed ***' + '\n');
+
+
+console.log('*** Remove recipe step2 ***');
 recipe.removeRecipeStep(2)
 recipe.displayRecipe();
+console.log('***Completed  removing recipe step2 ***' + '\n');
